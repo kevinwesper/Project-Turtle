edef360eefc1775dbf85bb748bd4b714914333a9
Youta14 <youta14@outlook.com>
2017-01-25 13:47:28 +0100

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -30,11 +30,18 @@ class Main extends Sprite
 			currentScreen.onDestroy();
 		}
 		switch ( which ) 
-		{
-			case ScreenTypes.Title:
+		{	case ScreenTypes.Title:
 				currentScreen = new TitleScreen();
-			case ScreenTypes.Game:
-				currentScreen = new GameScreen();
+			case ScreenTypes.Battle1:
+				currentScreen = new Battle1Screen();
+			case ScreenTypes.Battle2:
+				currentScreen = new Battle2Screen();
+			case ScreenTypes.Battle3:
+				currentScreen = new Battle3Screen();
+			case ScreenTypes.Battle4:
+				currentScreen = new Battle4Screen();
+			case ScreenTypes.Battle5:
+				currentScreen = new Battle5Screen();
 			case ScreenTypes.Credits:
 				currentScreen = new CreditsScreen();
 			case ScreenTypes.Cutscene1:
@@ -43,6 +50,26 @@ class Main extends Sprite
 				currentScreen = new Cutscene2Screen();
 			case ScreenTypes.Cutscene3:
 				currentScreen = new Cutscene3Screen();
+			case ScreenTypes.Cutscene4:
+				currentScreen = new Cutscene4Screen();
+			case ScreenTypes.Cutscene5:
+				currentScreen = new Cutscene5Screen();
+			case ScreenTypes.Cutscene6:
+				currentScreen = new Cutscene6Screen();
+			case ScreenTypes.Cutscene7:
+				currentScreen = new Cutscene7Screen();
+			case ScreenTypes.Cutscene8:
+				currentScreen = new Cutscene8Screen();
+			case ScreenTypes.Cutscene9:
+				currentScreen = new Cutscene9Screen();
+			case ScreenTypes.Cutscene10:
+				currentScreen = new Cutscene10Screen();
+			case ScreenTypes.Cutscene11:
+				currentScreen = new Cutscene11Screen();
+			case ScreenTypes.Cutscene12:
+				currentScreen = new Cutscene12Screen();
+			case ScreenTypes.Cutscene13:
+				currentScreen = new Cutscene13Screen();
 		}
 		addChild( currentScreen );
 		currentScreen.onLoad();
