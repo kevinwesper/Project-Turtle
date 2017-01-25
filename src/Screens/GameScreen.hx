2d26e90ab47ea213d5cc08959b6a624b2a283275
Youta14 <youta14@outlook.com>
2017-01-25 12:30:36 +0100

    Update GameScreen.hx

--- src/Screens/GameScreen.hx
+++ src/Screens/GameScreen.hx
@@ -12,7 +12,7 @@ import buttons.*;
 /**
  * Class for the Game Screen
  * 
- * @author Kevin Beijer
+ * @author Kevin
  */
 
 class GameScreen extends Screen
@@ -29,79 +29,23 @@ class GameScreen extends Screen
 	
 	override public function onLoad():Void
 	{
-	package screens;
-
-import flash.display.SimpleButton;
-import openfl.Assets;
-import openfl.display.Bitmap;
-import openfl.display.BitmapData;
-import openfl.display.Sprite;
-import openfl.events.Event;
-import flash.system.System;
-
-import buttons.*;
-
-/**
- * Class for the Title Screen
- * 
- * @author Kevin
- */
-
-class TitleScreen extends Screen
-{
-	public function new() 
-	{
-		super();
+	var toMenu:Button = new Button(Assets.getBitmapData("img/Button_Turtle_Up_Main.png"), 
+		Assets.getBitmapData("img/Button_Turtle_Over_Main.png"), 
+		Assets.getBitmapData("img/Button_Turtle_Down_Main.png"), 
+		"Menu", onMenuClick);
+		toMenu.y = 150;
+		toMenu.x = 674;
+		addChild( toMenu );
 		
-		var titleData:BitmapData = Assets.getBitmapData( "img/Starting screen_right size.png");
 
-		var title:Bitmap = new Bitmap( titleData );
-	
-		addChild(title);
-	}
-
-	override public function onLoad():Void
-	{
-		var toGame:Button = new Button(Assets.getBitmapData("img/Button_Turtle_Up_Start.png"), 
-		Assets.getBitmapData("img/Button_Turtle_Over_Start.png"), 
-		Assets.getBitmapData("img/Button_Turtle_Down_Start.png"), 
-		"Start", onPlayClick);
-		toGame.y = 150;
-		toGame.x = 344;
-		addChild( toGame );
-		
-		var toCredits:Button = new Button(Assets.getBitmapData("img/Button_Turtle_Up_Credits.png"), 
-		Assets.getBitmapData("img/Button_Turtle_Over_Credits.png"), 
-		Assets.getBitmapData("img/Button_Turtle_Down_Credits.png"), 
-		"Credits", onCreditsClick);
-		toCredits.y = 210;
-		toCredits.x = 344;
-		addChild( toCredits );
 		
 		var toQuit:Button = new Button(Assets.getBitmapData("img/Button_Turtle_Up_Quit.png"), 
 		Assets.getBitmapData("img/Button_Turtle_Over_Quit.png"), 
 		Assets.getBitmapData("img/Button_Turtle_Down_Quit.png"), 
 		"Quit game", onQuitClick);
 		toQuit.y = 270;
-		toQuit.x = 344;
+		toQuit.x = 674;
 		addChild( toQuit );
-	}
-
-	private function onPlayClick()
-	{
-		Main.instance.loadScreen( ScreenTypes.Cutscene1 );
-	}
-	
-	private function onCreditsClick()
-	{
-		Main.instance.loadScreen( ScreenTypes.Credits );
-	}
-	
-	private function onQuitClick()
-	{
-		Sys.exit(0);
-	}
-}
 	}
 
 	private function onMenuClick()
