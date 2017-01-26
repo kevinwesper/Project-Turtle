ab06a77a67eb1bcd4bf98b5bf3fb43d05475bfa5
kevinwesper <kevinbeijer@ziggo.nl>
2017-01-26 19:23:20 +0100

    Add files via upload

---
+++ src/PlacingCards.hx
@@ -0,0 +1,61 @@
+package; 
+
+import openfl.Assets; 
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.display.Sprite; 
+import screens.Battle1Screen;
+
+import openfl.events.MouseEvent;
+
+import motion.Actuate;
+
+/**
+ * Class for creating the playable cards on the screen.
+ * 
+ * @author Kevin Beijer
+ */
+
+class PlacingCards extends Sprite
+{
+	// the name of the table
+	var tableName:String;
+	
+	// the cards on the table
+	var cards:Array<Cards>; 
+	
+	// connecting to game screens
+	var dealer:screens.Battle1Screen;
+	
+	var deck:Array<Cards> = new Array<Cards>();
+	
+	var selection:Array<Cards> = new Array<Cards>();
+	
+	public function new( newTableName:String, mainRef:screens.Battle1Screen )
+	{ 
+		super();
+		
+		cards = new Array<Cards>(); 
+		tableName = newTableName;
+		dealer = mainRef;
+	}
+	
+	// Adds the card from the parameter to the cards array
+	public function addCard( newCard:Cards )
+	{ 
+		cards.push( newCard );
+		displayCards();
+	} 
+	
+	// To display cards
+	function displayCards()
+	{ 
+		removeChildren();
+		var xPos:Float = 0;
+		var yPos:Float = 0;
+		for ( card in cards )
+		{ 
+			addChild( card );
+		}
+	} 
+}
