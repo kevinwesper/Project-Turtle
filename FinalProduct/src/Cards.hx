3af91b20db7cbd36440afaa007f25719d2a25b66
kevinwesper <kevinbeijer@ziggo.nl>
2017-01-31 20:47:19 +0100

    Add files via upload

---
+++ FinalProduct/src/Cards.hx
@@ -0,0 +1,40 @@
+package; 
+
+import openfl.Assets; 
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.display.Sprite;
+
+/**
+ * Code for determining cards
+ * 
+ * @author Kevin Beijer & Georgi Georgiev
+ */
+
+class Cards extends Sprite
+{
+	public var type:String;
+	public var strength:Int;
+	public var healing:Int;
+	public var defence:Int;
+	public var imageName:String;
+	
+	var deck:Array<Cards> = new Array<Cards>();
+	
+	public function new( thisType:String, thisStrength:Int, thisHealing:Int, thisDefence:Int, thisImage:String )
+	{
+		super();
+		strength = thisStrength;
+		healing = thisHealing;
+		defence = thisDefence;
+		type = thisType;
+		imageName = thisImage;
+		
+		var cardData:BitmapData = Assets.getBitmapData( imageName );
+		var card:Bitmap = new Bitmap( cardData);
+		
+		card.scaleX = card.scaleY = 3 / 4;
+		
+		addChild( card );
+	}
+}
---
+++ FinalProduct/src/Main.hx
@@ -0,0 +1,103 @@
+package;
+
+import openfl.display.Sprite;
+import screens.*;
+import buttons.*;
+
+/**
+ * Class for the screens to change. For all the screens to come together and have a great time
+ * 
+ * @author Kevin Beijer & Deborah Oosterhuis
+ */
+
+class Main extends Sprite 
+{
+	private var currentScreen:Screen;
+	public static var instance(get, null):Main;
+
+	private function new () 
+	{
+		super ();
+	}
+
+	public function loadScreen( which:ScreenTypes )
+	{
+		if( currentScreen != null && contains( currentScreen ) )
+		{
+			removeChild( currentScreen );
+			currentScreen.onDestroy();
+		}
+		switch ( which ) 
+		{	
+			case ScreenTypes.Title:
+				currentScreen = new TitleScreen();
+			case ScreenTypes.Battle1:
+				currentScreen = new Battle1Screen();
+			case ScreenTypes.Battle2:
+				currentScreen = new Battle2Screen();
+			case ScreenTypes.Battle3:
+				currentScreen = new Battle3Screen();
+			case ScreenTypes.Battle4:
+				currentScreen = new Battle4Screen();
+			case ScreenTypes.Battle5:
+				currentScreen = new Battle5Screen();
+			case ScreenTypes.Disclaimer:
+				currentScreen = new DisclaimerScreen();
+			case ScreenTypes.Credits:
+				currentScreen = new CreditsScreen();
+			case ScreenTypes.Cutscene1:
+				currentScreen = new Cutscene1Screen();
+			case ScreenTypes.Cutscene2:
+				currentScreen = new Cutscene2Screen();
+			case ScreenTypes.Cutscene3:
+				currentScreen = new Cutscene3Screen();
+			case ScreenTypes.Cutscene4:
+				currentScreen = new Cutscene4Screen();
+			case ScreenTypes.Cutscene5:
+				currentScreen = new Cutscene5Screen();
+			case ScreenTypes.Cutscene6:
+				currentScreen = new Cutscene6Screen();
+			case ScreenTypes.Cutscene7:
+				currentScreen = new Cutscene7Screen();
+			case ScreenTypes.Cutscene8:
+				currentScreen = new Cutscene8Screen();
+			case ScreenTypes.Cutscene9:
+				currentScreen = new Cutscene9Screen();
+			case ScreenTypes.Cutscene10:
+				currentScreen = new Cutscene10Screen();
+			case ScreenTypes.Cutscene11:
+				currentScreen = new Cutscene11Screen();
+			case ScreenTypes.Cutscene12:
+				currentScreen = new Cutscene12Screen();
+			case ScreenTypes.Cutscene13:
+				currentScreen = new Cutscene13Screen();
+			case ScreenTypes.Dead1:
+				currentScreen = new Dead1Screen();
+			case ScreenTypes.Dead2:
+				currentScreen = new Dead2Screen();
+			case ScreenTypes.Dead3:
+				currentScreen = new Dead3Screen();
+			case ScreenTypes.Dead4:
+				currentScreen = new Dead4Screen();
+			case ScreenTypes.Dead5:
+				currentScreen = new Dead5Screen();
+		}
+		addChild( currentScreen );
+		currentScreen.onLoad();
+	}
+
+	public static function get_instance():Main
+	{
+		if( instance == null )
+			instance = new Main();
+		return instance;
+	}
+
+	public static function main()
+	{
+		var m:Main = Main.instance;
+		openfl.Lib.current.stage.addChild( m );
+
+		m.loadScreen( ScreenTypes.Title );
+	}
+}
